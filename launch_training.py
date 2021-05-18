b4d30987302afa1a056338b0dec15d22de84e9a2
AntoineRodriguez <rodrihuezantoine5@gmail.com>
2021-05-18 22:31:48 +0200

    update launch_training

--- launch_training.py
+++ launch_training.py
@@ -14,18 +14,19 @@ parser.add_argument("data", help="path to data file (.npz)")
 parser.add_argument("-d", "--distance", help="used distance (euclidean or cosinus)")
 parser.add_argument("-s", "--save", help="path to save the MixKmeans object if an error happen")
 
-data, dist, save = parser.parse_args()
+args = parser.parse_args()
 
 if __name__ == '__main__':
 
     # Chargement
-    dtm = sparse.load_npz(data)
+    dtm = sparse.load_npz(args.data)
 
     COST_HISTORIC = []
     for K in np.arange(50, 550, 50):
         print('begin fitting with {} clusters'.format(K))
-        model = MixKMeans(x=-3, weights=(0.2, 0.8), distance=dist, save_file=save)
+        model = MixKMeans(x=-3, weights=(0.2, 0.8), distance=args.distance, save_file=args.save)
         cost = model.fit(dtm, K, 50)
         COST_HISTORIC.append(cost)
 
         np.savez('', K, COST_HISTORIC)
+
