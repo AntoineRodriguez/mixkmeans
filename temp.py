8cbb759df532590cdc6670e60453dfe0bed6e8ba
AntoineRodriguez <rodrihuezantoine5@gmail.com>
2021-05-23 12:33:30 +0200

    little changes

--- temp.py
+++ temp.py
@@ -6,3 +6,9 @@ import pickle
 
 with open('notebooks/occ-cosin_save-100-567.pkl', 'rb') as file:
     objet = pickle.load(file)
+
+import numpy as np
+
+from scipy import sparse
+
+a = sparse.load_npz('../data/data_preprocess/dtm_occ.npz')
